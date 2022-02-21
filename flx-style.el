ec66c4d3f552a727640819cabb80efbd9dfcf47a
Jen-Chieh Shen <jcs090218@gmail.com>
2022-02-22 01:59:05 +0800

    Declare outward

--- flx-style.el
+++ flx-style.el
@@ -38,40 +38,41 @@
 (defvar flx-style-cache nil
   "Stores flx-cache.")
 
+(defun flx-style--commonality-hash (strs)
+  "Internal commonality, STRS."
+  (let* ((commonality-cache (make-hash-table :test 'equal :size 200))
+         (hash-value (gethash strs commonality-cache nil)))
+    (if hash-value
+        (if (eq hash-value 'nothing) nil hash-value)
+      (setq strs (mapcar #'string-to-list strs))
+      (let (res tried idx)
+        (dolist (char (car strs))
+          (unless (memq char tried)
+            (catch 'notfound
+              (setq idx (mapcar (lambda (str)
+                                  (or
+                                   (cl-position char str)
+                                   (throw 'notfound nil)))
+                                strs))
+              (push (cons char
+                          (fuzzy-commonality
+                           (cl-mapcar (lambda (str idx)
+                                        (cl-subseq str (1+ idx)))
+                                      strs idx)))
+                    res)
+              (push char tried))))
+        (setq res (if res
+                      (cl-reduce
+                       (lambda (a b)
+                         (if (> (length a) (length b)) a b))
+                       res)
+                    nil))
+        (puthash strs (or res 'nothing) commonality-cache)
+        res))))
+
 (defun flx-style-commonality (strs)
   "Return the largest string that fuzzy matches all STRS."
-  (cl-letf* ((commonality-cache (make-hash-table :test 'equal :size 200))
-             ((symbol-function #'fuzzy-commonality)
-              (lambda (strs)
-                (let ((hash-value (gethash strs commonality-cache nil)))
-                  (if hash-value
-                      (if (eq hash-value 'nothing) nil hash-value)
-                    (setq strs (mapcar #'string-to-list strs))
-                    (let (res tried idx)
-                      (dolist (char (car strs))
-                        (unless (memq char tried)
-                          (catch 'notfound
-                            (setq idx (mapcar (lambda (str)
-                                                (or
-                                                 (cl-position char str)
-                                                 (throw 'notfound nil)))
-                                              strs))
-                            (push (cons char
-                                        (fuzzy-commonality
-                                         (cl-mapcar (lambda (str idx)
-                                                      (cl-subseq str (1+ idx)))
-                                                    strs idx)))
-                                  res)
-                            (push char tried))))
-                      (setq res (if res
-                                    (cl-reduce
-                                     (lambda (a b)
-                                       (if (> (length a) (length b)) a b))
-                                     res)
-                                  nil))
-                      (puthash strs (or res 'nothing) commonality-cache)
-                      res))))))
-    (concat (fuzzy-commonality strs))))
+  (concat (flx-style--commonality-hash strs)))
 
 (defun flx-style-find-holes (merged str)
   "Find positions in MERGED, where insertion by the user is likely, wrt. STR"
